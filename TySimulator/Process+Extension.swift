e2e1a54860120b278bdd7be76bcee0929fd69874
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-13 14:34:16 +0800

    A Process+Extension

---
+++ TySimulator/Process+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Process+Extension.swift
+//  TySimulator
+//
+//  Created by luckytianyiyan on 2016/11/13.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+import Foundation
+
+extension Process {
+    static func output(launchPath: String, arguments: [String], directoryPath: URL? = nil) -> String {
+        let output = Pipe()
+        
+        let task = Process()
+        task.launchPath = launchPath
+        task.arguments = arguments
+        task.standardOutput = output
+        
+        if let path = directoryPath?.removeTrailingSlash.path {
+            task.currentDirectoryPath = path
+        }
+        
+        task.launch()
+        
+        // For some reason [task waitUntilExit]; does not return sometimes. Therefore this rather hackish solution:
+        var count = 0;
+        while task.isRunning && count < 10 {
+            Thread.sleep(forTimeInterval: 0.1)
+            count += 1
+        }
+        
+        let data = output.fileHandleForReading.readDataToEndOfFile()
+        guard let result = String(data: data, encoding: .utf8) else {
+            return ""
+        }
+        
+        return result
+    }
+}
