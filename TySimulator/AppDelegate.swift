4f3efc01d9dcf4b75bc5084f847e0a1960837624
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-15 01:12:08 +0800

    M configure HockeySDK

--- TySimulator/AppDelegate.swift
+++ TySimulator/AppDelegate.swift
@@ -7,17 +7,18 @@
 //
 
 import Cocoa
+import HockeySDK
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
+        BITHockeyManager.shared().configure(withIdentifier: "4809e9695f5749449758cf7ec79710f5")
+        BITHockeyManager.shared().start()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
