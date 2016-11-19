d402c4c05c527048247298b2e637199c9c1c64d9
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-20 03:10:50 +0800

    A ShortcutTableCellView

---
+++ TySimulator/Preferences/ShortcutTableCellView.swift
@@ -0,0 +1,14 @@
+//
+//  ShortcutTableCellView.swift
+//  TySimulator
+//
+//  Created by luckytianyiyan on 2016/11/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+import Cocoa
+import MASShortcut
+
+class ShortcutTableCellView: NSTableCellView {
+    @IBOutlet open var shortcutView: MASShortcutView?
+}
