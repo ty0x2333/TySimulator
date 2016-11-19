1fa2cad15e2325dd049aa7241275dff1b4444512
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-20 01:48:46 +0800

    A Command

---
+++ TySimulator/Model/CommandModel.swift
@@ -0,0 +1,15 @@
+//
+//  CommandModel.swift
+//  TySimulator
+//
+//  Created by luckytianyiyan on 2016/11/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+import Foundation
+
+class CommandModel {
+    var name: String = ""
+    var script: String = ""
+    var key: String = ""
+}
