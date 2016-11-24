073899a092d7c649f21a4796c9a90f2b992a1dcd
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-24 21:59:42 +0800

    A String+MD5

---
+++ TySimulator/TySimulator-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TySimulator-Bridging-Header.h
+//  TySimulator
+//
+//  Created by luckytianyiyan on 2016/11/24.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#ifndef TySimulator_Bridging_Header_h
+#define TySimulator_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h> // required for MD5
+
+
+#endif /* TySimulator_Bridging_Header_h */
