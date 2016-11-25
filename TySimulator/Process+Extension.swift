c07b59558ecb063369ca8a949b6a70c8be79bc4d
luckytianyiyan <luckytianyiyan@gmail.com>
2016-11-26 02:19:26 +0800

    M Process+Extension add transformedScript(_ script: String) -> String

--- TySimulator/Process+Extension.swift
+++ TySimulator/Process+Extension.swift
@@ -38,6 +38,49 @@ extension Process {
         return result
     }
     
+    public class func transformedScript(_ script: String) -> String {
+        var result = script
+        var res: [NSTextCheckingResult] = []
+        do {
+            let regex = try NSRegularExpression(pattern:"\\$\\{.*\\}", options:NSRegularExpression.Options.caseInsensitive)
+            res = regex.matches(in:script, options:NSRegularExpression.MatchingOptions(rawValue: 0), range:NSMakeRange(0, script.characters.count))
+        } catch {
+            log.error(error)
+        }
+        guard res.count > 0 else {
+            return script
+        }
+        for checkingRes in res {
+            var commandValue = (script as NSString).substring(with:checkingRes.range)
+            commandValue = (commandValue as NSString).substring(from: 1)
+            do {
+                if let command = try JSONSerialization.jsonObject(with: commandValue.data(using: .utf8)!, options: []) as? Dictionary<String, String> {
+                    if let deviceId = command["device"] {
+                        var device: DeviceModel?
+                        if deviceId != "booted" {
+                            device = Device.shared().device(udid: deviceId)
+                        } else {
+                            device = Device.bootedDevices().first
+                        }
+                        guard device != nil else {
+                            continue
+                        }
+                        if let bundleIdentifier = command["application"] {
+                            if let application = device?.application(bundleIdentifier: bundleIdentifier) {
+                                if let location = application.loadDataLocation()?.removeTrailingSlash.absoluteString {
+                                    result = (result as NSString).replacingCharacters(in: checkingRes.range, with: location)
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch {
+                log.error(error)
+            }
+        }
+        return result
+    }
+    
     static func execute(_ script: String) -> String {
         var scriptCLI = script
         if let device = Device.bootedDevices().first {
