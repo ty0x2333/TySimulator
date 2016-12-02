13130497c846340bad49f8c9ad1275d2764cde6f
luckytianyiyan <luckytianyiyan@gmail.com>
2016-12-03 00:36:58 +0800

    M GeneralPreferencesViewController add automaticallyChecksForUpdates

--- TySimulator/Preferences/GeneralPreferencesViewController.swift
+++ TySimulator/Preferences/GeneralPreferencesViewController.swift
@@ -14,6 +14,7 @@ class GeneralPreferencesViewController: NSViewController, MASPreferencesViewCont
     @IBOutlet weak var isOnlyHasContentDevices: NSButton!
     @IBOutlet weak var isOnlyAvailableDevices: NSButton!
     @IBOutlet weak var isLaunchAtStartup: NSButton!
+    @IBOutlet weak var isAutomaticallyChecksForUpdates: NSButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +22,7 @@ class GeneralPreferencesViewController: NSViewController, MASPreferencesViewCont
         self.isLaunchAtStartup.state = NSApplication.isLaunchAtStartup ? NSOnState : NSOffState
         self.isOnlyAvailableDevices.state = preference.onlyAvailableDevices ? NSOnState : NSOffState
         self.isOnlyHasContentDevices.state = preference.onlyHasContentDevices ? NSOnState : NSOffState
+        self.isAutomaticallyChecksForUpdates.state = DM_SUUpdater.shared().automaticallyChecksForUpdates ? NSOnState : NSOffState
     }
     @IBAction func onLaunchAtStartupButtonClicked(_ sender: NSButton) {
         NSApplication.isLaunchAtStartup = sender.state == NSOnState
@@ -32,6 +34,10 @@ class GeneralPreferencesViewController: NSViewController, MASPreferencesViewCont
     
     @IBAction func onOnlyHasContentDevicesButtonClicked(_ sender: NSButton) {
         Preference.shared().onlyHasContentDevices = sender.state == NSOnState
+    }
+    
+    @IBAction func onAutomaticallyChecksForUpdatesButtonClicked(_ sender: NSButton) {
+        DM_SUUpdater.shared().automaticallyChecksForUpdates = sender.state == NSOnState
     }
     
     // MARK: MASPreferencesViewController
